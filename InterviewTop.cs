0d32b69eb1c46b8eacd64e26895f4fba34513644
Who6off <ivanguzikxv@gmail.com>
2022-08-31 00:38:24 -0400

    Two sum

---
+++ InterviewTop.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+    internal static class InterviewTop
+    {
+        //https://leetcode.com/problems/two-sum/
+        static public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return new int[0];
+        }
+
+
+
+    }
+}
